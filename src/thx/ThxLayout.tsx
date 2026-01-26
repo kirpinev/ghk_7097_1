4fe2496f14ff022536f824608d56985869967f44
Кирпинев Игорь Валерьевич <IKirpinev@alfabank.ru>
2026-01-26 17:03:02 +0300

    init

---
+++ src/thx/ThxLayout.tsx
@@ -0,0 +1,55 @@
+import { Typography } from "@alfalab/core-components/typography";
+import moai from "../assets/bigSmile.png";
+import { thxSt } from "./style.css";
+import { appSt } from "../style.css.ts";
+import { ButtonMobile } from "@alfalab/core-components/button/mobile";
+import { Gap } from "@alfalab/core-components/gap";
+import { useEffect } from "react";
+
+export const ThxLayout = () => {
+  const submit = () => {
+    window.gtag("event", "7079_get_real_sub", {
+      variant_name: "ghk_7079_1",
+    });
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundColor = "white !important";
+  }, []);
+
+  return (
+    <>
+      <div className={thxSt.container} style={{ backgroundColor: "white" }}>
+        <img
+          alt="Картинка ракеты"
+          src={moai}
+          width={250}
+          className={thxSt.rocket}
+        />
+        <Typography.TitleResponsive
+          font="system"
+          tag="h1"
+          view="large"
+          weight="bold"
+        >
+          Такого вы точно не ожидали
+        </Typography.TitleResponsive>
+        <Gap size={8} />
+        <Typography.Text tag="p" view="primary-medium">
+          Вы только что поучаствовали в настоящем эксперименте. Сервиса не
+          существует. Мы скоро предложим лучшие условия!
+        </Typography.Text>
+      </div>
+      <div className={appSt.bottomBtn}>
+        <ButtonMobile
+          block
+          view="primary"
+          href="alfabank://multistep-route?fromModule=FORM&stepNumber=0&alias=alfa-subscription-alias&version=2"
+          onClick={submit}
+        >
+          Спасибо, понятно!
+        </ButtonMobile>
+      </div>
+    </>
+  );
+};
